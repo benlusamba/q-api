f8327ee3cd3cad074d7c2cdff06c34557f2df697
Ben Lusamba <benlusamba@gmail.com>
2018-04-19 14:45:39 -0400

    Typo.

--- quanco.py
+++ quanco.py
@@ -4,7 +4,7 @@ import quandl
 import pandas as pd
 import csv                        # Allows importation/exporation of data as .csv file
 
-gdp = quandl.get("FRED/GDP", authtoken="_uhAD1-g9JdY2teUwiAb")   # Import data using API call and applicable rules
+gdp = quandl.get("FRED/GDP", authtoken="YOUR_API_KEY")   # Import data using API call and applicable rules
 
 print(gdp)                        # Show data in workspace
 
