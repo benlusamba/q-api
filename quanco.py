763bc48f2299d02872275e4536c839175955691d
Ben Lusamba <benlusamba@gmail.com>
2018-04-23 15:50:55 -0400

    API Specs.

--- quanco.py
+++ quanco.py
@@ -4,7 +4,7 @@ import quandl
 import pandas as pd
 import csv                        # Allows importation/exporation of data as .csv file
 
-gdp = quandl.get("FRED/GDP", authtoken="YOUR_API_KEY")   # Import data using API call and applicable rules
+gdp = quandl.get("FRED/GDP", authtoken="INSERT_KEY")   # Import data using API call and applicable rules
 
 print(gdp)                        # Show data in workspace
 
